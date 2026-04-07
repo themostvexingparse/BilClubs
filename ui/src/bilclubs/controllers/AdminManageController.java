941d7193df74b7f54e1d1de9e66ac9f899a5e4f4
themostvexingparse <kerbalgalactic@gmail.com>
2026-04-08 02:18:15 +0300

    club card layout fix

--- ui/src/bilclubs/controllers/AdminManageController.java
+++ ui/src/bilclubs/controllers/AdminManageController.java
@@ -14,6 +14,8 @@ import bilclubs.components.MemberCard;
 import bilclubs.components.SearchResultPane;
 import bilclubs.utils.RequestManager;
 import bilclubs.utils.Response;
+import bilclubs.utils.LoadingSign;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.layout.VBox;
 
@@ -23,69 +25,100 @@ public class AdminManageController {
     @FXML VBox eventVBox;
 
     @FXML
-    public void initialize() throws IOException{
+    public void initialize() throws IOException {
+        LoadingSign loadClubs = new LoadingSign();
+        LoadingSign loadMembers = new LoadingSign();
+        LoadingSign loadEvents = new LoadingSign();
         
-        JSONObject request = new JSONObject();
-        request.put("action", "listClubs");
-        request.put("userId", Controller.userId);
-        request.put("sessionToken", Controller.sessionToken);
-        Response allClubs = RequestManager.sendPostRequest("api/user", request);
-        JSONArray allClubsJSONArray = allClubs.getPayload().getJSONArray("clubs");
+        loadClubs.showLoadingIcon(clubVBox);
+        loadMembers.showLoadingIcon(memberVBox);
+        loadEvents.showLoadingIcon(eventVBox);
 
-        for(Object obj : allClubsJSONArray){
-            JSONObject aClub = (JSONObject)obj;
-            BanClubCard newest = new BanClubCard(aClub);
-            clubVBox.getChildren().add(newest);
-        }
+        new Thread(() -> {
+            try {
+                JSONObject request = new JSONObject();
+                request.put("action", "listClubs");
+                request.put("userId", Controller.userId);
+                request.put("sessionToken", Controller.sessionToken);
+                Response allClubsResponse = RequestManager.sendPostRequest("api/user", request);
+                final JSONArray allClubsJSONArray = allClubsResponse.getPayload().optJSONArray("clubs") != null ? allClubsResponse.getPayload().getJSONArray("clubs") : new JSONArray();
 
-        ArrayList<Integer> allClubIds = new ArrayList<>();
+                ArrayList<Integer> allClubIds = new ArrayList<>();
+                final JSONArray membersToRender = new JSONArray();
+                ArrayList<Integer> noDuplication = new ArrayList<>();
 
-        ArrayList<Integer> noDuplication = new ArrayList<>();
+                for (int i = 0; i < allClubsJSONArray.length(); i++) {
+                    JSONObject currentClub = allClubsJSONArray.getJSONObject(i);
+                    int clubId = currentClub.getInt("id");
+                    allClubIds.add(clubId);
 
-        for (Object obj : allClubsJSONArray) {
-            JSONObject currentClub = (JSONObject) obj;
-            int clubId = currentClub.getInt("id");
+                    JSONObject JSONmember = new JSONObject();
+                    JSONmember.put("action", "getMembers");
+                    JSONmember.put("userId", Controller.userId);
+                    JSONmember.put("sessionToken", Controller.sessionToken);
+                    JSONmember.put("clubId", clubId);
 
-            JSONObject JSONmember = new JSONObject();
-            JSONmember.put("action", "getMembers");
-            JSONmember.put("userId", Controller.userId);
-            JSONmember.put("sessionToken", Controller.sessionToken);
-            JSONmember.put("clubId", clubId);
+                    Response memberListResponse = RequestManager.sendPostRequest("api/club", JSONmember);
+                    JSONArray members = memberListResponse.getPayload().optJSONArray("members");
+                    if (members != null) {
+                        for (int mIndex = 0; mIndex < members.length(); mIndex++) {
+                            JSONObject aMember = members.getJSONObject(mIndex);
+                            int memberId = aMember.getInt("id");
 
-            Response memberListResponse = RequestManager.sendPostRequest("api/club", JSONmember);
-            System.out.println(memberListResponse);
-            JSONArray members = memberListResponse.getPayload().getJSONArray("members");
-
-            boolean add = true;
-            for (Object m : members) {
-                JSONObject aMember = (JSONObject) m;
-                int memberId = aMember.getInt("id");
-
-                if (!noDuplication.contains(memberId)) {
-                    noDuplication.add(memberId);
-                    MemberCard thisCard = new MemberCard(aMember);
-                    memberVBox.getChildren().add(thisCard);
+                            if (!noDuplication.contains(memberId)) {
+                                noDuplication.add(memberId);
+                                membersToRender.put(aMember);
+                            }
+                        }
+                    }
                 }
-                
+
+                if (allClubIds.isEmpty()) allClubIds.add(-1);
+
+                JSONObject JSONevent = new JSONObject();
+                JSONevent.put("action", "getUpcomingEvents");
+                JSONevent.put("userId", Controller.userId);
+                JSONevent.put("sessionToken", Controller.sessionToken);
+                JSONevent.put("clubIds", new JSONArray(allClubIds));
+
+                Response eventsResponse = RequestManager.sendPostRequest("api/user", JSONevent);
+                final JSONArray eventList = eventsResponse.getPayload().optJSONArray("events") != null ? eventsResponse.getPayload().getJSONArray("events") : new JSONArray();
+
+                Platform.runLater(() -> {
+                    try {
+                        loadClubs.removeLoadingIcon(clubVBox);
+                        loadMembers.removeLoadingIcon(memberVBox);
+                        loadEvents.removeLoadingIcon(eventVBox);
+
+                        for (int i = 0; i < allClubsJSONArray.length(); i++) {
+                            JSONObject aClub = allClubsJSONArray.getJSONObject(i);
+                            try {
+                                clubVBox.getChildren().add(new BanClubCard(aClub));
+                            } catch (Exception e) {}
+                        }
+
+                        for (int i = 0; i < membersToRender.length(); i++) {
+                            JSONObject aMember = membersToRender.getJSONObject(i);
+                            try {
+                                memberVBox.getChildren().add(new MemberCard(aMember));
+                            } catch (Exception e) {}
+                        }
+
+                        for (int i = 0; i < eventList.length(); i++) {
+                            JSONObject anEvent = eventList.getJSONObject(i);
+                            try {
+                                eventVBox.getChildren().add(new BanEventCard(anEvent));
+                            } catch (Exception e) {}
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                });
+
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-        }
-
-        JSONObject JSONevent = new JSONObject();
-        JSONevent.put("action", "getUpcomingEvents");
-        JSONevent.put("userId", Controller.userId);
-        JSONevent.put("sessionToken", Controller.sessionToken);
-        JSONevent.put("clubIds", new JSONArray(allClubIds));
-
-        Response events = RequestManager.sendPostRequest("api/user", JSONevent);
-        JSONArray eventList = events.getPayload().getJSONArray("events");
-
-        for(Object obj : eventList){
-            JSONObject anEvent = (JSONObject)obj;
-
-            BanEventCard aPane = new BanEventCard(anEvent);
-            eventVBox.getChildren().add(aPane);
-        }
-
+        }).start();
 
     }
 }
--- ui/src/bilclubs/controllers/ClubController.java
+++ ui/src/bilclubs/controllers/ClubController.java
@@ -9,9 +9,11 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 
 import javafx.event.ActionEvent;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
+import bilclubs.utils.LoadingSign;
 import javafx.scene.control.Label;
 import javafx.scene.control.Menu;
 import javafx.scene.image.Image;
@@ -56,135 +58,174 @@ public class ClubController {
 
     @FXML
     public void initialize() throws IOException {
-        JSONObject request = new JSONObject();
-        request.put("action", "listClubs");
-        request.put("userId", Controller.userId);
-        request.put("sessionToken", Controller.sessionToken);
-        Response allClubs = RequestManager.sendPostRequest("api/user", request);
+        LoadingSign loadEvents = new LoadingSign();
+        loadEvents.showLoadingIcon(happeningSoonVBox);
 
-        JSONArray allClubsJSONArray = allClubs.getPayload().getJSONArray("clubs");
+        new Thread(() -> {
+            try {
+                JSONObject request = new JSONObject();
+                request.put("action", "listClubs");
+                request.put("userId", Controller.userId);
+                request.put("sessionToken", Controller.sessionToken);
+                Response allClubs = RequestManager.sendPostRequest("api/user", request);
 
-        for (Object obj : allClubsJSONArray) {
-            JSONObject club = (JSONObject) obj;
-            if (club.getInt("id") == Controller.currentClubId) {
-                clubNameLbl.setText(club.getString("clubName"));
-                clubDescLbl.setText(club.getString("clubDescription").split("\n")[0]);
-                memberCountLabel.setText(club.getInt("memberCount") + " members");
+                final JSONArray allClubsJSONArray = allClubs.getPayload().optJSONArray("clubs") != null ? allClubs.getPayload().getJSONArray("clubs") : new JSONArray();
 
-                int clubPrivilege = club.optInt("clubPrivilege", -1);
-                boolean isMember = clubPrivilege != -1 && clubPrivilege != 0; // 0 = not a member or banned
-                boolean isManager = clubPrivilege > 1 && isMember;  //manager / admin
-
-                if (isMember) {
-                    joinButton.setVisible(false);
-                    joinButton.setDisable(true);
-                    leaveButton.setVisible(true);
-                    leaveButton.setDisable(false);
-
-                } else {
-                    leaveButton.setVisible(false);
-                    leaveButton.setDisable(true);
-                    joinButton.setVisible(true);
-                    joinButton.setDisable(false);
-                    
-
+                JSONObject currentClubData = null;
+                for (int i = 0; i < allClubsJSONArray.length(); i++) {
+                    JSONObject club = allClubsJSONArray.getJSONObject(i);
+                    if (club.getInt("id") == Controller.currentClubId) {
+                        currentClubData = club;
+                        break;
+                    }
                 }
-                if (isManager) {
-                    manageButton.setVisible(true);
-                    manageButton.setDisable(false);
-                }
+                final JSONObject finalCurrentClubData = currentClubData;
 
-                // Load club icon (profile image) and banner (cover) from the server.
-                // The filenames already contain the "static/" prefix, so we just
-                // append them to the server base address.
-                String baseUrl = bilclubs.utils.RequestManager.defaultAddress;
-                String iconFilename = club.optString("iconFilename", "");
-                String coverFilename = club.optString("coverFilename", "");
+                JSONObject eventRequest = new JSONObject();
+                eventRequest.put("action", "getUpcomingEvents");
+                eventRequest.put("clubIds", new JSONArray().put(Controller.currentClubId));
+                eventRequest.put("userId", Controller.userId);
+                eventRequest.put("sessionToken", Controller.sessionToken);
 
-                if (!iconFilename.isEmpty()) {
-                    Image icon = new Image(baseUrl + iconFilename, true); // true = background loading
-                    clubProfileImage.setImage(icon);
-                    clubProfileImage.setPreserveRatio(true);
-                }
+                Response eventResponse = RequestManager.sendPostRequest("api/user", eventRequest);
+                final JSONArray eventData = eventResponse.getPayload().optJSONArray("events") != null ? eventResponse.getPayload().getJSONArray("events") : new JSONArray();
 
-                if (!coverFilename.isEmpty()) {
-                    Image cover = new Image(baseUrl + coverFilename, true);
-                    clubBanner.setImage(cover);
-                    clubBanner.setPreserveRatio(false); // stretch to fill banner area
-                }
+                Platform.runLater(() -> {
+                    try {
+                        loadEvents.removeLoadingIcon(happeningSoonVBox);
+                        if (finalCurrentClubData != null) {
+                            clubNameLbl.setText(finalCurrentClubData.getString("clubName"));
+                            clubDescLbl.setText(finalCurrentClubData.getString("clubDescription").split("\n")[0]);
+                            memberCountLabel.setText(finalCurrentClubData.getInt("memberCount") + " members");
 
-                break;
+                            int clubPrivilege = finalCurrentClubData.optInt("clubPrivilege", -1);
+                            boolean isMember = clubPrivilege != -1 && clubPrivilege != 0; // 0 = not a member or banned
+                            boolean isManager = clubPrivilege > 1 && isMember;  //manager / admin
+
+                            if (isMember) {
+                                joinButton.setVisible(false);
+                                joinButton.setDisable(true);
+                                leaveButton.setVisible(true);
+                                leaveButton.setDisable(false);
+                            } else {
+                                leaveButton.setVisible(false);
+                                leaveButton.setDisable(true);
+                                joinButton.setVisible(true);
+                                joinButton.setDisable(false);
+                            }
+                            if (isManager) {
+                                manageButton.setVisible(true);
+                                manageButton.setDisable(false);
+                            }
+
+                            String baseUrl = bilclubs.utils.RequestManager.defaultAddress;
+                            String iconFilename = finalCurrentClubData.optString("iconFilename", "");
+                            String coverFilename = finalCurrentClubData.optString("coverFilename", "");
+
+                            if (!iconFilename.isEmpty()) {
+                                Image icon = new Image(baseUrl + iconFilename, true); // true = background loading
+                                clubProfileImage.setImage(icon);
+                                clubProfileImage.setPreserveRatio(true);
+                            }
+
+                            if (!coverFilename.isEmpty()) {
+                                Image cover = new Image(baseUrl + coverFilename, true);
+                                clubBanner.setImage(cover);
+                                clubBanner.setPreserveRatio(false); // stretch to fill banner area
+                            }
+                        }
+
+                        if (eventData.length() == 0) {
+                            upcomingEventsLabel.setText("No upcoming events for now.");
+                        }
+
+                        for (int i = 0; i < eventData.length(); i++) {
+                            JSONObject eventObj = eventData.getJSONObject(i);
+                            try {
+                                SearchResultPane event = new SearchResultPane(eventObj, "Event");
+                                happeningSoonVBox.getChildren().add(event);
+                            } catch (Exception e) {}
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                });
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-        }
-
-        // eventleri alıcaz
-
-        JSONObject eventRequest = new JSONObject();
-        eventRequest.put("action", "getUpcomingEvents");
-        eventRequest.put("clubIds", new JSONArray().put(Controller.currentClubId));
-        eventRequest.put("userId", Controller.userId);
-        eventRequest.put("sessionToken", Controller.sessionToken);
-
-        Response eventResponse = RequestManager.sendPostRequest("api/user", eventRequest);
-        JSONArray eventData = eventResponse.getPayload().getJSONArray("events");
-
-        if (eventData.length() == 0) {
-            upcomingEventsLabel.setText("No upcoming events for now.");
-        }
-
-        for (Object obj : eventData) {
-            JSONObject eventObj = (JSONObject) obj;
-            SearchResultPane event = new SearchResultPane(eventObj, "Event");
-            happeningSoonVBox.getChildren().add(event);
-        }
+        }).start();
     }
 
     public void joinClub(ActionEvent e) throws IOException {
-        JSONObject joinRequest = new JSONObject();
-        joinRequest.put("action", "joinClub");
-        joinRequest.put("clubId", Controller.currentClubId);
-        joinRequest.put("userId", Controller.userId);
-        joinRequest.put("sessionToken", Controller.sessionToken);
+        joinButton.setDisable(true);
+        new Thread(() -> {
+            try {
+                JSONObject joinRequest = new JSONObject();
+                joinRequest.put("action", "joinClub");
+                joinRequest.put("clubId", Controller.currentClubId);
+                joinRequest.put("userId", Controller.userId);
+                joinRequest.put("sessionToken", Controller.sessionToken);
 
-        Response joinResponse = RequestManager.sendPostRequest("api/user", joinRequest);
-        System.out.println(joinResponse);
+                Response joinResponse = RequestManager.sendPostRequest("api/user", joinRequest);
+                
+                Platform.runLater(() -> {
+                    if (joinResponse.isSuccess()) {
+                        joinButton.setVisible(false);
+                        joinButton.setDisable(true);
+                        leaveButton.setVisible(true);
+                        leaveButton.setDisable(false);
 
-        if (joinResponse.isSuccess()) {
-            joinButton.setVisible(false);
-            joinButton.setDisable(true);
-
-            leaveButton.setVisible(true);
-            leaveButton.setDisable(false);
-
-            NotificationCard joinNotification = new NotificationCard(NotificationCard.joinMessage, "You joined " + Controller.currentClubName);
-            AlertsController.allNotifs.add(joinNotification);
-        }
+                        try {
+                            NotificationCard joinNotification = new NotificationCard(NotificationCard.joinMessage, "You joined " + Controller.currentClubName);
+                            AlertsController.allNotifs.add(joinNotification);
+                        } catch (Exception ex) {}
+                    } else {
+                        joinButton.setDisable(false);
+                    }
+                });
+            } catch (Exception ex) {
+                ex.printStackTrace();
+                Platform.runLater(() -> joinButton.setDisable(false));
+            }
+        }).start();
     }
 
     public void leaveClub(ActionEvent e) throws IOException {
-        JSONObject leaveRequest = new JSONObject();
-        leaveRequest.put("action", "leaveClub");
-        leaveRequest.put("clubId", Controller.currentClubId);
-        leaveRequest.put("userId", Controller.userId);
-        leaveRequest.put("sessionToken", Controller.sessionToken);
+        leaveButton.setDisable(true);
+        new Thread(() -> {
+            try {
+                JSONObject leaveRequest = new JSONObject();
+                leaveRequest.put("action", "leaveClub");
+                leaveRequest.put("clubId", Controller.currentClubId);
+                leaveRequest.put("userId", Controller.userId);
+                leaveRequest.put("sessionToken", Controller.sessionToken);
 
-        Response leaveResponse = RequestManager.sendPostRequest("api/user", leaveRequest);
-        System.out.println(leaveResponse);
+                Response leaveResponse = RequestManager.sendPostRequest("api/user", leaveRequest);
 
-        if (leaveResponse.isSuccess()) {
-            leaveButton.setVisible(false);
-            leaveButton.setDisable(true);
+                Platform.runLater(() -> {
+                    if (leaveResponse.isSuccess()) {
+                        leaveButton.setVisible(false);
+                        leaveButton.setDisable(true);
 
-            joinButton.setVisible(true);
-            joinButton.setDisable(false);
+                        joinButton.setVisible(true);
+                        joinButton.setDisable(false);
 
-            manageButton.setVisible(false);
-            manageButton.setDisable(true);
+                        manageButton.setVisible(false);
+                        manageButton.setDisable(true);
 
-            NotificationCard leaveNotification = new NotificationCard(NotificationCard.leaveMessage, "Check your webmail for details.");
-            AlertsController.allNotifs.add(leaveNotification);
-
-        }
+                        try {
+                            NotificationCard leaveNotification = new NotificationCard(NotificationCard.leaveMessage, "Check your webmail for details.");
+                            AlertsController.allNotifs.add(leaveNotification);
+                        } catch (Exception ex) {}
+                    } else {
+                        leaveButton.setDisable(false);
+                    }
+                });
+            } catch (Exception ex) {
+                ex.printStackTrace();
+                Platform.runLater(() -> leaveButton.setDisable(false));
+            }
+        }).start();
     }
 
     public void goToManagement(ActionEvent e) throws IOException {
