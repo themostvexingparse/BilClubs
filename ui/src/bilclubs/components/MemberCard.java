ba4fe2eb33b88bce6ba65c619f4fafda1d1fc9c9
themostvexingparse <kerbalgalactic@gmail.com>
2026-04-12 03:47:36 +0300

    event posters/banners

--- ui/src/bilclubs/components/MemberCard.java
+++ ui/src/bilclubs/components/MemberCard.java
@@ -13,11 +13,10 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import bilclubs.controllers.Controller;
 
-public class MemberCard extends HBox {
+public class MemberCard extends Pane {
 
     @FXML
     Label namelbl;
